7775eb3414005d85ec926be8599c12e93f6390f2
Gregory Losco <gtlosco@gmail.com>
2019-09-16 21:55:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,4 +31,12 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+  
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
 end
