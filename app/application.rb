08939a0daa612c1dd53589bf6371fb7cdab04f99
Gregory Losco <gtlosco@gmail.com>
2019-09-16 22:04:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
       end
     end
     elsif req.path.match(/add/)
-      added_item = req.params["q"]
+      added_item = req.params["item"]
     else
       resp.write "Path Not Found"
     end
